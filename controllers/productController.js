28bdbe8f4ed5f22a488b9db5b9292a90d6c69c51
Shanosha <wootenm1@gmail.com>
2026-03-20 14:49:02 -0400

    bugfix/updated responses and removed uneeded comments

--- controllers/productController.js
+++ controllers/productController.js
@@ -22,10 +22,10 @@ const createProduct = async (req, res) => {
     }
     try {
         const newProduct = await Product.create(req.body)
-        console.log(`Product with id ${newProduct._id} created.`)
-        res.status(201).json({message: `Product with id ${newProduct._id} created.`})
+        res.status(201).json(newProduct)
     }
     catch(err) {
+        console.error(err.message)
         res.status(400).json({message: err.message})
     }
 }
@@ -45,7 +45,6 @@ const getProducts = async (req, res) => {
             .sort(sort)
             .skip(skip)
             .limit(limit)
-        console.log(`Products found (${products.length}): `, products)
         res.status(200).json(products)
     }
     catch(err) {
@@ -58,13 +57,14 @@ const getProductById = async (req, res) => {
     try {
         const id = req.params.id
         const product = await Product.find({_id: id})
-        if(!product){ throw new Error(`No product with id ${id} found.`)}
-        console.log(`Product with id ${product._id} found.`, product)
+        if (!product) {
+            return res.status(404).json({ message: `No product with id ${id} found.`})
+        }
         res.status(200).json(product)
     }
     catch(err) {
         console.error(err.message)
-        res.status(404).json({message: err.message})
+        res.status(500).json({message: err.message})
     }
 }
 
@@ -80,13 +80,14 @@ const replaceProductById = async (req, res) => {
     try {
         const id = req.params.id
         const updatedProduct = await Product.findOneAndReplace({_id: id}, req.body, {new: true, runValidators:true})
-        if(!updatedProduct){ throw new Error(`No product with id ${id} found.`)}
-        console.log(`Product with id ${updatedProduct._id} updated.`, updatedProduct)
-        res.status(200).json({message: `Product with id ${updatedProduct._id} updated.`})
+        if (!updatedProduct) {
+            return res.status(404).json({ message: `No product with id ${id} found.`})
+        }
+        res.status(200).json(updatedProduct)
     }
     catch(err) {
         console.error(err.message)
-        res.status(404).json({message: err.message})
+        res.status(400).json({message: err.message})
     }
 }
 
@@ -94,14 +95,14 @@ const deleteProductById = async (req, res) => {
     try {
         const id = req.params.id
         const deletedProduct = await Product.findByIdAndDelete(id)
-        if(!deletedProduct){ throw new Error(`No product with id ${id} found.`)}
-        console.log(`Product with id ${deletedProduct._id} found.`, deletedProduct)
-        res.status(200).json({message: `Product with id ${deletedProduct._id} deleted.`})
+        if (!deletedProduct) {
+            return res.status(404).json({ message: `No product with id ${id} found.`})
+        }
+        res.status(200).json({message: `Product with id ${deletedBook._id} deleted.`})
     }
     catch(err) {
-        console.error(err)
         console.error(err.message)
-        res.status(404).json({message: err.message})
+        res.status(500).json({message: err.message})
     }
 }
 
